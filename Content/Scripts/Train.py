01595a1ec90344e708c6cb3846d01fd0e1914a5c
ComedianxD <61299112+ComedianxD@users.noreply.github.com>
2020-02-23 19:42:33 +0200

    Update Train.py
    Updated names of variables
    and the access of hp dictionary keys

--- Content/Scripts/Train.py
+++ Content/Scripts/Train.py
@@ -6,10 +6,8 @@ import pickle
 class Game:
     def __init__(self):
         self.actions = {0: 'MoveToPlayer', 1: 'DodgeRight', 2: 'DodgeLeft', 3: 'DodgeBack', 4: 'Attack', 5: 'Idle'}
-        self.play_healths = {100: 0, 95: 1, 90: 2, 85: 3, 80: 4, 75: 5, 70: 6, 65: 7, 60: 8, 55: 9, 50: 10, 45: 11, 40: 12, 35: 13, 30: 14, 25: 15, 20: 16, 15: 17, 10: 18, 5: 19, 0: 20}
-        self.enemy_healths = {100: 0, 95: 1, 90: 2, 85: 3, 80: 4, 75: 5, 70: 6, 65: 7, 60: 8, 55: 9, 50: 10, 45: 11, 40: 12, 35: 13, 30: 14, 25: 15, 20: 16, 15: 17, 10: 18, 5: 19, 0: 20}
-        self.play_healths_inv = {0: 100, 1: 95, 2: 90, 3: 85, 4: 80, 5: 75, 6: 70, 7: 65, 8: 60, 9: 55, 10: 50, 11: 45, 12: 40, 13: 35, 14: 30, 15: 25, 16: 20, 17: 15, 18: 10, 19: 5, 20: 0}
-        self.enemy_healths_inv = {0: 100, 1: 95, 2: 90, 3: 85, 4: 80, 5: 75, 6: 70, 7: 65, 8: 60, 9: 55, 10: 50, 11: 45, 12: 40, 13: 35, 14: 30, 15: 25, 16: 20, 17: 15, 18: 10, 19: 5, 20: 0}
+        self.npc_hps = {0: 0, 5: 1, 10: 2, 15: 3, 20: 4, 25: 5, 30: 6, 35: 7, 40: 8, 45: 9, 50: 10, 55: 11, 60: 12, 65: 13, 70: 14, 75: 15, 80: 16, 85: 17, 90: 18, 95: 19, 100: 20}
+        self.opp_hps = {0: 0, 5: 1, 10: 2, 15: 3, 20: 4, 25: 5, 30: 6, 35: 7, 40: 8, 45: 9, 50: 10, 55: 11, 60: 12, 65: 13, 70: 14, 75: 15, 80: 16, 85: 17, 90: 18, 95: 19, 100: 20}
         self.distances = {0: '1500 - 800', 1: '800 - 500', 2: '500 - 200', 3: '200 - 0'}
 
         # hit_reward = 20
@@ -26,39 +24,39 @@ class Game:
         self.moves_counter = 0
         self.is_attacking = False
 
-        self.q_table2 = np.random.uniform(low=0, high=5, size=(len(self.play_healths), len(self.enemy_healths), len(self.distances), len(self.actions)))
+        self.q_table2 = np.random.uniform(low=0, high=5, size=(len(self.npc_hps), len(self.opp_hps), len(self.distances), len(self.actions)))
         # print(q_table2.ndim)
         # print(q_table2.size)
         # print(q_table2.shape)
         self.q_table2[:, :, :2, 4] = -float('inf') #!Magdi!#
         self.q_table2[:, :, 3, 4] = 10
         self.q_table2[:, :, 2, 4] = 3
-        ue.print_string("Q_Table Class : Constructor")
+        # ue.print_string("Q_Table Class : Constructor")
 
     def intialize_states(self, cur_old_e_o_hp_dist):
-        ue.print_string(f"Iterator :=> {self.iterator}, Epislon :=> {self.epsilon}")
+        ue.print_string(f"Iterator :=> {self.iterator}, Epsilon :=> {self.epsilon}")
         if self.iterator == self.episodes:
             return -1
     
         L = cur_old_e_o_hp_dist.split(',')
-        c_p_health = int(L[0])
-        c_e_health = int(L[1])
-        c_dist = int(L[2])
-        o_p_health = int(L[3])
-        o_e_health = int(L[4])
-        o_dist = int(L[5])
+        curr_npc_hp = int(L[0])
+        curr_opp_hp = int(L[1])
+        curr_dist = int(L[2])
+        old_npc_hp = int(L[3])
+        old_opp_hp = int(L[4])
+        old_dist = int(L[5])
         self.is_attacking = bool(L[6])
 
-        if c_p_health <= 0:
-            c_p_health = 0
-        if c_e_health <= 0:
-            c_e_health = 0
-        if o_p_health <= 0:
-            o_p_health = 0
-        if o_e_health <= 0:
-            o_e_health = 0
+        if curr_npc_hp <= 0:
+            curr_npc_hp = 0
+        if curr_opp_hp <= 0:
+            curr_opp_hp = 0
+        if old_npc_hp <= 0:
+            old_npc_hp = 0
+        if old_opp_hp <= 0:
+            old_opp_hp = 0
 
-        state = (c_p_health, c_e_health, c_dist, o_p_health, o_e_health, o_dist)
+        state = (curr_npc_hp, curr_opp_hp, curr_dist, old_npc_hp, old_opp_hp, old_dist)
         old_distance_index = 0
         if 1500 >= state[5] > 800:
             old_distance_index = 0
@@ -79,9 +77,9 @@ class Game:
         elif 200 >= state[2] > 0:
             old_distance_index = 3
 
-        old_state = (self.play_healths[state[3]], self.enemy_healths[state[4]], old_distance_index)
-        new_state = (self.play_healths[state[0]], self.enemy_healths[state[1]], new_distance_index)
-        ue.print_string(f"Old State {old_state}, New State {new_state}")
+        old_state = (self.npc_hps[state[3]], self.opp_hps[state[4]], old_distance_index)
+        new_state = (self.npc_hps[state[0]], self.opp_hps[state[1]], new_distance_index)
+        # ue.print_string(f"Old State {old_state}, New State {new_state}")
 
         self.calc_reward(new_state, old_state)
         action = self.take_action(new_state[0], new_state[1], new_state[2])
@@ -92,10 +90,10 @@ class Game:
         index_state = (p_health, e_health, d_stance)
 
         if np.random.random() > self.epsilon:
-            ue.print_string("Take Max Q_Value")
+            # ue.print_string("Take Max Q_Value")
             action = np.argmax(self.q_table2[index_state])
         else:
-            ue.print_string("Explore: Random Action")
+            # ue.print_string("Explore: Random Action")
             action = np.random.randint(0, 6)
         return action
 
@@ -103,24 +101,27 @@ class Game:
         self.iterator += 1
         if self.iterator % self.decay_every == 0 and self.iterator > self.decay_from:
             self.epsilon *= self.eps_decay
+            ue.print_string("DECAY")
             
         self.save_table()
         
     def calc_reward(self, current_state, old_state):
-        if self.enemy_healths_inv[old_state[1]] - self.enemy_healths_inv[current_state[1]] == 0:
+        if old_state[1] * 5 - current_state[1] * 5 == 0:
             self.moves_counter += 1
 
+        action = self.take_action(old_state[0], old_state[1], old_state[2])
+
         succ_dodge = 0
-        if self.is_attacking and current_state[2] >= 200:
+        if self.is_attacking is True and current_state[2] <= 200 and (action == 1 or action == 2 or action == 3) and old_state[1] * 5 - current_state[1] * 5 == 0:
             succ_dodge = 15
+            ue.print_string(f"successful dodge {self.is_attacking}")
 
-        reward = (self.enemy_healths_inv[old_state[1]] - self.enemy_healths_inv[current_state[1]]) - (self.play_healths_inv[old_state[0]] - self.play_healths_inv[current_state[0]]) - (
-                self.moves_counter * 0.22) + succ_dodge
+        reward = (old_state[1] * 5 - current_state[1] * 5) - (old_state[0] * 5 - current_state[0] * 5) - (self.moves_counter * 0.22) + succ_dodge
+        ue.print_string(f"Reward: {reward}")
 
-        if self.enemy_healths_inv[old_state[1]] - self.enemy_healths_inv[current_state[1]] != 0:
+        if old_state[1] * 5 - current_state[1] * 5 != 0:
             self.moves_counter = 0
 
-        action = self.take_action(old_state[0], old_state[1], old_state[2])
         max_future_q = np.max(self.q_table2[old_state])
         current_q = self.q_table2[old_state][action]
         new_q = (1 - self.learn_rate) * current_q + self.learn_rate * (reward + self.discount * max_future_q)
@@ -131,19 +132,19 @@ class Game:
         filename = r'Q_Table.pickle'
         with open(filename, 'wb') as f:
             pickle.dump(self.q_table2, f)
-            ue.print_string(f"{self.iterator} : Saved Q_Table")
+            # ue.print_string(f"{self.iterator} : Saved Q_Table")
         filename = r'Episode.pickle'
         with open(filename, 'wb') as f:
             pickle.dump(self.iterator, f)
-            ue.print_string(f"{self.iterator} : Saved Episode")
+            # ue.print_string(f"{self.iterator} : Saved Episode")
 
     def load_table(self):
         filename = r'Q_Table.pickle'
         with open(filename, 'rb') as f:
             self.q_table2 = pickle.load(f)
-            ue.print_string(f"{self.iterator} : Saved Q_Table")
+            # ue.print_string(f"{self.iterator} : Load Q_Table")
         filename = r'Episode.pickle'
         with open(filename, 'rb') as f:
             self.iterator = pickle.load(f)
-            ue.print_string(f"{self.iterator} : Load Episode")
+            # ue.print_string(f"{self.iterator} : Load Episode")
         return self.iterator
